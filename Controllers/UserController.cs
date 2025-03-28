e77772b0adc87a6602b9d6aee62ab4a3be1d5a07
userforfun201 <ngocanhit201@gmail.com>
2025-03-28 16:33:46 +0700

    init

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenerateEntity.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		[HttpGet]
+		public async Task<IActionResult> Test()
+		{
+			return Ok("");
+
+		}
+	}
+}
